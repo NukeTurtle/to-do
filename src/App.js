29a2654a390e189cf40baf3303d7e69d23cd48ac
steelsnake93 <nikolanikotasev@protonmail.com>
2023-09-28 22:13:02 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
 import React from "react";
-// import ListProjects from "./components/ListProjects";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Layout from "./pages/Layout";
-// import Home from "./pages/Home";
-// import Todos from "./pages/Todos";
 
 function App() {
-  return (
-    <>
-      <div className="App">
-        {/* <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="ListTodos" element={<ListProjects/>}></Route>
-            <Route path="Projects" element={<Todos />} />
-          </Route>
-        </Routes>
-        </BrowserRouter> */}
-      </div>
-    </>
-  );
+  return (<div className="App"></div>);
 }
 
 export default App;
--- src/pages/Todos.jsx
+++
@@ -1,9 +0,0 @@
-import ListTodos from "../components/ListTodos";
-
-const Todos = () => {
-    return (
-        <ListTodos></ListTodos>
-    )
-  };
-  
-  export default Todos;
