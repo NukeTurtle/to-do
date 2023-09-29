dd408832145c252b2fde6b3464814ad5e8ac1504
NukeTurtle <stiglic.elvis@gmail.com>
2023-09-29 20:02:44 +0300

    uncomment previous work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import ListProjects from "./components/ListProjects";
+import ListProjects from "./components/ListProjects";
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Layout from "./pages/Layout";
 // import Home from "./pages/Home";
